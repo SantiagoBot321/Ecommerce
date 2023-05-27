666311fe7a4c006a21c96973eb37c9715fac7166
SantiagoBot321 <boteroramirezsantiago@gmail.com>
2023-05-26 20:46:58 -0500

    creando base de datos y generando el grid

--- vite-project/src/components/ProductGrid.jsx
+++ vite-project/src/components/ProductGrid.jsx
@@ -1,38 +1,24 @@
 import React from 'react'
-import productDatabase from './product-database.js'
+import productDatabase from '../database/product-database'
+
+
 
 const ProductGrid = () => {
   return (
-    <section id="product-grid">
-      <h2>Productos</h2>
+    <section>
       <ul>
-        <li>
-          <picture>
-            <img />
-          </picture>
-          <h3><span>Nombre del producto</span></h3>
-          <h3><span>Precio</span></h3>
-          <h3>Añadir al carrito</h3>
-        </li>
-      </ul>
-    </section>
-  )
-}
-
-const ProductGrid = ({ addToCart }) => {
-  return (
-    <div>
       {productDatabase.map((product) => (
-        <div key={product.id}>
-          <img src={product.image} alt={product.name} />
-          <h3>{product.name}</h3>
+        <li key={product.id}>
+          <img src={product.img} alt={product.nameProduct} />
+          <h3>{product.nameProduct}</h3>
           <p>${product.price}</p>
           <button onClick={() => addToCart(product)}>
             Añadir al carrito
           </button>
-        </div>
+        </li>
       ))}
-    </div>
+      </ul>
+    </section>
   );
 };
 
--- vite-project/src/database/product-database.js
+++ vite-project/src/database/product-database.js
@@ -1,4 +1,4 @@
-export const data = [
+const data = [
 	{
 		id: 1,
 		img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
@@ -35,3 +35,5 @@ export const data = [
 		quantity: 1,
 	},
 ];
+
+export default data;
