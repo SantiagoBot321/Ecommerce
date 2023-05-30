58d3a5034d47b7c7296e5138c7e57111b261a190
ycmegar <yc.mesa03@gmail.com>
2023-05-29 21:48:55 -0500

    Configuraciones login

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -1,27 +1,46 @@
-import React, { useContext } from 'react'
-import UserContext from '../context/UserContext'
-import loginStyles from '../styles/Login.module.css'
+import React, { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+import PageContext from "../context/PageContext";
 
-useContext
 const Login = () => {
-  const ingreso = (evento) => {
-    evento.preventDefault() 
-  }
+  const { login, userError } = useContext(UserContext);
+  const {change} = useContext(PageContext)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const access = login(email, password);
+    setEmail("");
+    setPassword("");
+    console.log("access", access);
+    if (access) {change(null)}
+  };
+
   return (
-    <>
-      <form className={loginStyles.form} action='#' onSubmit={ingreso}>
-        <fieldset>
-          <input type="text" placeholder='usuario' id='usuario' name='usuario'/>
-        </fieldset>
-        <fieldset>
-          <input type="text" placeholder='contraseña' id='clave' name='clave'/>
-          </fieldset>
-        <fieldset>
-          <button type='submit'>ingresar</button>
-        </fieldset>
-      </form>
-    </>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Iniciar sesión</button>
+      {userError ? <p>{userError}</p> : null}
+    </form>
+  );
+};
 
 export default Login;
--- vite-project/src/providers/UserProvider.jsx
+++ vite-project/src/providers/UserProvider.jsx
@@ -1,19 +1,52 @@
 import { useEffect, useState } from "react";
 import UserContext from "../context/UserContext";
 
-const UserProvider = ({children}) => {
-    const [user, set] = useState(null);
-     const login = function(username, password){
-        fetch().then(respuesta =>respuesta.json).then(usuarios =>{
-            //Verificación usuarios
-            return set({username});
-        })
-        .catch((error)=>console.error(error))    
-    } 
-    const save = function(username, password) {
-        fetch(url, {});
-    }
-    return <UserContext.Provider value={{user, set, login, save }}>
-        {children}</UserContext.Provider>;
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [userError, setUserError] = useState(null);
+  const apiUrl = "https://6474c4567de100807b1bb4ed.mockapi.io/users";
+
+  const login = (email, password) => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((usuarios) => {
+        const foundUser = usuarios.find(
+          (usuario) => usuario.email === email && usuario.password === password
+        );
+        console.log(foundUser);
+        if (foundUser) {
+          setUser(foundUser);
+          return foundUser;
+        } else {
+            setUserError("Credenciales Invalidas");
+            return false;
+        }
+      })
+      .catch((error) => error);
+  };
+
+  const save = (email, password) => {
+    const newUser = { email, password };
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data); // Guardar el usuario devuelto por la API en el estado
+        console.log("Usuario guardado:", data);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser, login, save, userError }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
+
 export default UserProvider;
