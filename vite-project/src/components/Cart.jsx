90fb43805e63afae7e8ca3ce1d23634bcb7691d9
ycmegar <yc.mesa03@gmail.com>
2023-05-30 20:11:15 -0500

    Cart.jsx

---
+++ vite-project/src/components/Cart.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import CartContext from '../context/CartContext';
+
+const Cart = () => {
+  const { cartItems, removeFromCart, clearCart } = useContext(CartContext);
+
+  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+
+  return (
+    <section>
+      <h2>Carrito de compras</h2>
+      {cartItems && cartItems.length > 0 ? (
+        <div>
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id}>
+                <h3>{item.nameProduct}</h3>
+                <p>Precio: ${item.price}</p> {/* Añadirle cantidad*/}
+                <button onClick={() => removeFromCart(item.id)}>Eliminar</button>
+              </li>
+            ))}
+          </ul>
+          <p>Valor total: ${totalPrice}</p> {/ Mostrar el valor total */}
+        </div>
+      ) : (
+        <p>No hay productos en el carrito.</p>
+      )}
+      {cartItems && cartItems.length > 0 && (
+        <button onClick={clearCart}>Vaciar carrito</button>
+      )}
+    </section>
+  );
+};
+
+export default Cart;
