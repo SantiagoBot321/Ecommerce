4f63f2511590af21dae7b2491b3500800cb76ac6
SantiagoBot321 <boteroramirezsantiago@gmail.com>
2023-05-30 20:18:11 -0500

    Modificando Login

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -1,51 +1,52 @@
-import React, { useState, useContext } from "react";
+import React, { useContext, useState } from "react";
 import UserContext from "../context/UserContext";
 import PageContext from "../context/PageContext";
 
 const Login = () => {
-  const { login, userError } = useContext(UserContext);
-  const {change} = useContext(PageContext)
+  const { login, userError, setUser } = useContext(UserContext);
+  const { change } = useContext(PageContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    login(email, password)
-      .then((access) => {
-        setEmail("");
-        setPassword("");
-        console.log("access", access);
-        if (access) {
-          change("productGrid"); // Cambiar el estado de la página a "purchases"
-        }
-      })
-      .catch((error) => console.error(error));
+    try {
+      await login(email, password);
+      setEmail("");
+      setPassword("");
+      console.log("Inicio de sesión exitoso");
+      change(null);
+    } catch (error) {
+      console.error("Error de inicio de sesión:", error);
+    }
   };
-  
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Iniciar sesión</button>
-      {userError ? <p>{userError}</p> : null}
-    </form>
+    <section>
+      <h2>Inicia sesión</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Iniciar sesión</button>
+        {userError && <p>{userError}</p>}
+      </form>
+    </section>
   );
 };
 
