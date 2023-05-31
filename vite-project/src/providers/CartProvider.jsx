e648c1e184fac6cd3cee39ba467e374520d6440a
ycmegar <yc.mesa03@gmail.com>
2023-05-30 20:12:13 -0500

    CartProvider correct

--- vite-project/src/providers/CartProvider.jsx
+++ vite-project/src/providers/CartProvider.jsx
@@ -1,35 +1,34 @@
-import CartContext from "../context/CartContext";
-import { useState,useEffect } from "react";
-
+import CartContext from '../context/CartContext';
+import { useState, useEffect } from 'react';
 
 const CartProvider = ({ children }) => {
-        const [cartItems, setCartItems] = useState(null);
-      
-        const addToCart = (products) => {
-          setCartItems([...cartItems, products]);
-        };
-      
-        const removeFromCart = (productId) => {
-          const updatedCartItems = cartItems.filter((item) => item.id !== productId);
-          setCartItems(updatedCartItems);
-        };
-      
-        const clearCart = () => {
-          setCartItems([]);
-        };
-      
-      
-    return (
-        <CartContext.Provider
-          value={{
-            cartItems,
-            addToCart,
-            removeFromCart,
-            clearCart,
-          }}
-        >
-          {children}
-        </CartContext.Provider>
-      );
-        }
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== productId);
+    setCartItems(updatedCartItems);
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
 export default CartProvider;
