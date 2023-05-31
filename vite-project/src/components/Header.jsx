77ef078da146b80dc7d43b3133976ff3ecde239e
Juanfer55 <juanfersierra07@gmail.com>
2023-05-30 20:21:36 -0500

    header changes

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,28 +1,49 @@
 import React, { useContext } from "react";
 import headerStyle from '../styles/Header.module.css';
 import PageContext from "../context/PageContext";
-import logo from '../assets/Logo-minimal.png'
+import UserContext from "../context/UserContext";
+import logo from '../assets/Logo-minimal.png';
+import CartContext from "../context/CartContext";
 
 const Header = () => {
-  const {change} = useContext(PageContext)
+  const { change } = useContext(PageContext);
+  const { user, setUser } = useContext(UserContext);
+  const { cartItems } = useContext(CartContext);
+
+  const handleLogout = () => {
+    setUser(null); 
+  };
+
+  const cartItemCount = cartItems ? cartItems.length : 0;
+
   return (
-      <header className={headerStyle.cont}>
-        <picture onClick={() => change(null)}>
-          <img className={headerStyle.logo}src={logo} alt="logo-header" />
-        </picture>
-        <ul className={headerStyle.nav} >
-          <li className={headerStyle.boton} onClick={() => change('save')}>
-            <p>Registrarse</p>
-          </li>
-          <li className={headerStyle.boton} onClick={() => change('login')}> 
-            <p>Login</p>
-          </li>
-          <li className={headerStyle.boton} onClick={() => change('Mi carrito')}>
-            <p>Mi carrito</p>
-          </li>
-        </ul>
-      </header>
-    );
+    <header className={headerStyle.cont}>
+      <picture onClick={() => change(null)}>
+        <img className={headerStyle.logo} src={logo} alt="logo-header" />
+      </picture>
+      <ul className={headerStyle.nav}>
+        {!user ? (
+          <>
+            <li className={headerStyle.boton} onClick={() => change('save')}>
+              <p>Registrarse</p>
+            </li>
+            <li className={headerStyle.boton} onClick={() => change('login')}>
+              <p>Login</p>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className={headerStyle.boton} onClick={() => change('Mi carrito')}>
+              <p>Mi carrito ({cartItemCount})</p>
+            </li>
+            <li className={headerStyle.boton} onClick={handleLogout}>
+              <p>Cerrar sesión</p>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
