5969fde8f5a347459cc9af25d67cc53cde6ba10f
SantiagoBot321 <boteroramirezsantiago@gmail.com>
2023-06-01 20:13:35 -0500

    modificando cart

--- vite-project/src/components/Cart.jsx
+++ vite-project/src/components/Cart.jsx
@@ -2,25 +2,19 @@ import React, { useContext } from 'react';
 import CartContext from '../context/CartContext';
 
 const Cart = () => {
-  const { cartItems, removeFromCart, clearCart } = useContext(CartContext);
-
-  const productCounts = {};
-  cartItems.forEach(item => {
-    if (productCounts[item.id]) {
-      productCounts[item.id]++;
-    } else {
-      productCounts[item.id] = 1;
-    }
-  });
+  const { cartItems, removeFromCart, clearCart, increaseQuantity, decreaseQuantity } = useContext(CartContext);
 
   const uniqueCartItems = [];
   let totalPrice = 0;
 
   cartItems.forEach(item => {
-    if (!uniqueCartItems.find(uniqueItem => uniqueItem.id === item.id)) {
-      uniqueCartItems.push(item);
-      totalPrice += item.price * productCounts[item.id];
+    const existingItem = uniqueCartItems.find(uniqueItem => uniqueItem.id === item.id);
+    if (existingItem) {
+      existingItem.quantity += item.quantity;
+    } else {
+      uniqueCartItems.push({ ...item });
     }
+    totalPrice += item.price * item.quantity;
   });
 
   return (
@@ -28,17 +22,16 @@ const Cart = () => {
       <h2>Carrito de compras</h2>
       {cartItems && cartItems.length > 0 ? (
         <ul>
-          <ul>
-            {uniqueCartItems.map((item) => (
-              <li key={item.id}>
-                <h3>{item.nameProduct}</h3>
-                <p>Precio: ${item.price}</p>
-                <p>Cantidad: {productCounts[item.id]}</p>
-                <button onClick={() => removeFromCart(item.id)}>Eliminar</button>
-              </li>
-            ))}
-          </ul>
-          <p>Valor total: ${totalPrice}</p>
+          {uniqueCartItems.map((item) => (
+            <li key={item.id}>
+              <h3>{item.nameProduct}</h3>
+              <p>Precio: ${item.price}</p>
+              <p>Cantidad: {item.quantity}</p>
+              <button onClick={() => removeFromCart(item.id)}>Eliminar</button>
+              <button onClick={() => increaseQuantity(item.id)}>+</button>
+              <button onClick={() => decreaseQuantity(item.id)}>-</button>
+            </li>
+          ))}
         </ul>
       ) : (
         <p>No hay productos en el carrito.</p>
@@ -46,6 +39,7 @@ const Cart = () => {
       {cartItems && cartItems.length > 0 && (
         <button onClick={clearCart}>Vaciar carrito</button>
       )}
+      <p>Valor total: ${totalPrice.toFixed(3)}</p> 
     </section>
   );
 };
